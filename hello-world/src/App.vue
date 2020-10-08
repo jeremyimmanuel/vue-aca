e9207c1c6338638dc1023f8833f45af8c82ce34d
Jeremy Tandjung <jeremytandjung@icloud.com>
2020-10-07 20:24:59 -0700

    hello world

---
+++ hello-world/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <div class="demo"
+        @click="attachedRed = !attachedRed"
+        :class="{ red: attachedRed }"></div>
+    <div class="demo green"></div>
+    <div class="demo blue"></div>
+    
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data: function() {
+    return {
+      attachedRed: false,
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.demo {
+  width: 100px;
+  height: 100px;
+  background-color: gray;
+  display: inline-block;
+  margin: 10px;
+}
+
+.red {
+  background-color: red;
+}
+
+.green {
+  background-color: green;
+}
+
+.blue {
+  background-color: blue;
+}
+</style>
