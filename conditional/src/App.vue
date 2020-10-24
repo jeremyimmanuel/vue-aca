0b84e32c54df0a578fcaeb0c5436726355b8f883
jeremyimmanuel <jeremytandjung@icloud.com>
2020-10-24 16:06:03 -0700

    update

---
+++ conditional/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <p v-if="show">You can see me!</p>
+    <p v-else>Now you see me!</p>
+
+    <template v-if="show">
+      <h1>Template</h1>
+      <p>Inside a template</p>
+    </template>
+    <p v-show="show">Do you see me?</p>
+
+    <button @click="show = !show">switch</button>
+    <vue-list/>
+  </div>
+</template>
+
+<script>
+import VueList from "./components/List"
+
+export default {
+  name: 'App',
+  components: {
+    VueList
+  },
+
+  data: function() {
+    return {
+      show: true
+    }
+  }
+
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ conditional/src/components/List.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="(ingredient, i) in ingredients" :key="ingredient">
+                {{ ingredient }} ({{ i }})
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "VueList",
+    data: function() {
+        return {
+            ingredients: ['meat', 'fruit', 'cookies'],
+            people: [
+                {name: 'Jeremy', age: 22, color: 'blue'},
+                {name: 'Jing', age: 21, color: 'purple'}
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
